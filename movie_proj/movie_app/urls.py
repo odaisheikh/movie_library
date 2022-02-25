7b1c822aca46e49f0bc2c2754ccf1c37b58b3a5c
odai sheikh <odaior300210@gmail.com>
2022-02-25 02:36:43 +0200

    add paths

--- movie_proj/movie_app/urls.py
+++ movie_proj/movie_app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path     
 from . import views
 urlpatterns = [
-    path('main_page/', views.main),	   
+    path('main_page/', views.main),   
+    path('favorit/', views.favorit),   
+    path('movie/', views.movie),   
+    path('watch_list/', views.watch_list),  
 ]
--- movie_proj/movie_app/views.py
+++ movie_proj/movie_app/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 
 def main(request):
     return render(request,"main_page.html")
+
+def favorit(request):
+    return render(request,"favorites.html")
+
+def movie(request):
+    return render(request,"movie.html")
+
+def watch_list(request):
+    return render(request,"watch_list.html")
