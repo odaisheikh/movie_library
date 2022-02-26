b5ffa5e444390e910f54dec262466e00e07f27b8
odai sheikh <odaior300210@gmail.com>
2022-02-26 09:49:56 +0200

    change some adjust

--- movie_proj/movie_app/urls.py
+++ movie_proj/movie_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('main_page/', views.main),   
-    path('favorit/', views.favorit),   
+    path('favorite/', views.favorit),   
     path('movie/', views.movie),   
     path('watch_list/', views.watch_list),  
 ]
