4c23086953aa6d4da631bf64dd2c1688ce92cfee
Ahmad Abedalqader <abedalqader-ahmad@hotmail.com>
2022-02-26 21:19:35 +0200

    applying jQuery and ajax on searchbox

--- movie_proj/movie_app/urls.py
+++ movie_proj/movie_app/urls.py
@@ -7,25 +7,15 @@ urlpatterns = [
     path('watch_list/', views.watch_list),
     path('adding_form', views.adding_form),
     path('add_movie', views.add_movie),
-<<<<<<< HEAD
-    path('category/<categ>', views.classify),
-    path('movies/search', views.search),
-    path('movies/search_result', views.search_result),
-    path('rate/<movie_id>', views.rate)
-=======
-
     path('rate/<movie_id>', views.rate),
     path('add_to_watchlist/<movie_id>', views.add_to_watchlist),
     path('add_to_favorites/<movie_id>', views.add_to_favorites),
     path('remove_from_watchlist/<movie_id>', views.remove_from_watchlist),
     path('remove_from_favorites/<movie_id>', views.remove_from_favorites),
-
-
     path('category/<categ>', views.classify),
     path('movies/search', views.search),
-    path('movies/search_result', views.search_result),
+    path('movies/search_result', views.search_result, name="search_result"),
+    path('rate/<movie_id>', views.rate),
+    path('my_movies', views.my_movies),
 
-    path('rate/<movie_id>', views.rate)
-
->>>>>>> dc910ba0d2354191881b7ee2be2281748f3bea17
 ]
